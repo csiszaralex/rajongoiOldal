fc438f51fe6d76ba38de1f9436044e94a80bca76
borbelybalint <36307758+ThisIsJustAGuy@users.noreply.github.com>
2021-01-05 11:25:25 +0100

    :lipstick: UI and code overhaul

--- client/app.js
+++ client/app.js
@@ -1,57 +1,144 @@
 $(document).ready(function () {
-    let onceRun = false;
-    axios.get('http://localhost:5000/api')
+  $(".page").load("kezdolap.html");
+  $("#kezdolap").click(function () {
+    $(".page").load("kezdolap.html");
+    changeActiveTab("kezdolap");
+  });
+
+  $("#bongeszes").click(function () {
+    $(".page").load("bongeszes.html", filterButton);
+
+    changeActiveTab("bongeszes");
+
+    fillFromDatabase();
+  });
+
+  $("#keszitok").click(function () {
+    $(".page").load("keszitok.html");
+    changeActiveTab("keszitok");
+  });
+});
+
+function changeActiveTab(tab) {
+  let navs = document.querySelectorAll(".nav-link");
+  navs.forEach((x) => {
+    if (x.id == tab) {
+      x.classList.add("active");
+    } else {
+      x.classList.remove("active");
+    }
+  });
+}
+
+function fillFromDatabase() {
+  let onceRun = false;
+  axios
+    .get("http://localhost:5000/api")
+    .then(function (response) {
+      response.data.forEach((element) => {
+        let div = document.createElement("div");
+        div.classList.add("card", "col-md-6");
+        div.innerHTML =
+          '<div class="card-body"> <h5 class="card-title">ID: </h5> <p class="card-subtitle text-muted">' +
+          element._id +
+          '</p><br /><p class="card-text">' +
+          "Típus: " +
+          element.tipus +
+          "<br />" +
+          "Cím: " +
+          element.cim +
+          "<br />" +
+          "Hossz: " +
+          element.hossz +
+          "<br />" +
+          "Kiadás: " +
+          element.kiadas +
+          "</p></div>";
+        document.querySelector(".content").appendChild(div);
+        if (!onceRun) {
+          onceRun = true;
+          selectFill(element);
+        }
+      });
+    })
+    .catch(function () {
+      console.log("baj van tesó");
+    });
+}
+
+function selectFill(ele) {
+  var separators = [":", ",", "{", "}"];
+  for (
+    let index = 1;
+    index <
+    JSON.stringify(ele).split(new RegExp(separators.join("|"), "g")).length - 1;
+    index += 2
+  ) {
+    let option = document.createElement("option");
+    option.text = JSON.stringify(ele)
+      .split(new RegExp(separators.join("|"), "g"))
+      [index].replace('"', "")
+      .replace('"', "");
+    document.querySelector("#selectMain").appendChild(option);
+  }
+}
+
+function filterButton() {
+  $("#btnMain").click(function () {
+    let selected = document.querySelector("#selectMain").value;
+    let userInput = document.querySelector("#textMain").value;
+    document.querySelector("#textMain").value = "";
+    document.querySelector(".content").innerHTML = "";
+    if (userInput != "") {
+      axios
+        .get(`http://localhost:5000/api/?${selected}=${userInput}`)
         .then(function (response) {
-            console.log(response.data);
-            response.data.forEach(element => {
-                let li = document.createElement("li");
-                li.innerHTML = "Id: " + element._id + "<br />" + "Típus: " + element.tipus + "<br />" + "Cím: " + element.cim + "<br />" + "Hossz: " + element.hossz + "<br />" + "Kiadás: " + element.kiadas + "<hr>";
-                document.querySelector(".content").appendChild(li);
-                if (!onceRun) {
-                    onceRun = true;
-                    selectFill(element);
-                }
-            });
+          response.data.forEach((element) => {
+            let div = document.createElement("div");
+            div.classList.add("card", "col-md-6");
+            div.innerHTML =
+              '<div class="card-body"> <h5 class="card-title">ID: </h5> <p class="card-subtitle text-muted">' +
+              element._id +
+              '</p><br /><p class="card-text">' +
+              "Típus: " +
+              element.tipus +
+              "<br />" +
+              "Cím: " +
+              element.cim +
+              "<br />" +
+              "Hossz: " +
+              element.hossz +
+              "<br />" +
+              "Kiadás: " +
+              element.kiadas +
+              "</p></div>";
+            document.querySelector(".content").appendChild(div);
+          });
         })
         .catch(function () {
-            console.log("baj van tesó");
-        })
-    function selectFill(ele) {
-        var separators = [':', ',', "{", "}"];
-        for (let index = 1; index < JSON.stringify(ele).split(new RegExp(separators.join('|'), 'g')).length - 1; index += 2) {
-            let option = document.createElement("option");
-            option.text = JSON.stringify(ele).split(new RegExp(separators.join('|'), 'g'))[index].replace('"', "").replace('"', "");
-            document.querySelector("#selectMain").appendChild(option);
-        }
+          console.log("baj van tesó");
+        });
+    } else {
+      fillFromDatabase();
     }
-    $("#btnMain").click(function () {
-        let selected = document.querySelector("#selectMain").value;
-        let userInput = document.querySelector("#textMain").value;
-        document.querySelector(".content").innerHTML = "";
-        axios.get(`http://localhost:5000/api/?${selected}=${userInput}`)
-            .then(function (response) {
-                response.data.forEach(element => {
-                    let li = document.createElement("li");
-                    li.innerHTML = "Id: " + element._id + "<br />" + "Típus: " + element.tipus + "<br />" + "Cím: " + element.cim + "<br />" + "Hossz: " + element.hossz + "<br />" + "Kiadás: " + element.kiadas + "<hr>";
-                    document.querySelector(".content").appendChild(li);
-                });
-            })
-            .catch(function () {
-                console.log("baj van tesó");
-            })
-    })
-    $("#btnNew").click(function () {
-        axios.post("http://localhost:5000/api", {
-            "tipus": document.querySelector("#newTipus").value.toString(),
-            "cim": document.querySelector("#newCim").value.toString(),
-            "hossz": document.querySelector("#newHossz").value.toString(),
-            "kiadas": document.querySelector("#newKiadas").value.toString()
-        })
-            .then(function () {
-                console.log("kész");
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    })
-})
+  });
+  addNewButton();
+}
+
+function addNewButton() {
+  $("#btnNew").click(function () {
+    axios
+      .post("http://localhost:5000/api", {
+        tipus: document.querySelector("#newTipus").value.toString(),
+        cim: document.querySelector("#newCim").value.toString(),
+        hossz: document.querySelector("#newHossz").value.toString(),
+        kiadas: document.querySelector("#newKiadas").value.toString(),
+      })
+      .then(function () {
+        console.log("kész");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+}
