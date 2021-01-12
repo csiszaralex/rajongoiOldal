66183b3c8986f12a2a7c257ace73ade6ac907d0c
borbelybalint <bbalint0404@gmail.com>
2021-01-12 11:46:08 +0100

    :construction: Bongeszes ongoing

---
+++ client/src/components/bongeszes/BongeszesCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card-body">
+    <h5 class="card-title">ID:</h5>
+    <p class="card-subtitle text-muted">{{ id }}</p>
+    <br />
+    <p class="card-text">
+      Típus: {{ tipus }}
+      <br />
+      Cím: {{ cim }}
+      <br />
+      Hossz: {{ hossz }}
+      <br />
+      Kiadás: {{ kiadas }}
+      <br />
+      Szavazatok: {{ szavazatok }}
+    </p>
+    <button type="button" class="btn btn-primary vote" :id="id">Szavazás</button>
+    <button
+      type="button"
+      onclick="DeleteButtons(this.id)"
+      class="btn vote btn-outline-danger mx-2"
+      style="width:100px"
+      :id="id"
+    >
+      Törlés
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  props: ['id', 'tipus', 'cim', 'hossz', 'kiadas', 'szavazatok']
+  //tipus, cim, hossz, kiadas, hozzaad, adatok
+};
+</script>
---
+++ client/src/views/Bongeszes.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-8">
+        <div v-for="adat in adatok" :key="adat._id" class="col-md-6 card"></div>
+      </div>
+      <div class="col-lg-4">
+        <h3 class="display-4 mt-5">Hozzáadás:</h3>
+        <p class="lead m-2">Típus:</p>
+        <input v-model="tipus" type="number" class="m-2 form-control" />
+        <p class="lead m-2">Cím:</p>
+        <input v-model="cim" type="text" class="m-2 form-control" />
+        <p class="lead m-2">Hossz:</p>
+        <input v-model="hossz" type="number" class="m-2 form-control" />
+        <p class="lead m-2">Kiadás:</p>
+        <input v-model="kiadas" type="number" class="m-2 form-control" />
+        <input
+          @click="hozzaad"
+          type="button"
+          class="m-2 btn btn-outline-success"
+          value="Hozzáadás"
+          id="btnNew"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+export default {
+  setup() {
+    const tipus = ref('');
+    const cim = ref('');
+    const hossz = ref('');
+    const kiadas = ref('');
+    function hozzaad() {
+      axios
+        .post('http://localhost:5000/api', {
+          tipus: tipus.value,
+          cim: cim.value,
+          hossz: hossz.value,
+          kiadas: kiadas.value
+        })
+        .then(function() {
+          console.log('kész');
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+    let adatok = ref({});
+    lekerdez();
+    function lekerdez() {
+      axios
+        .get('http://localhost:5000/api', {})
+        .then(function(response) {
+          adatok.value = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+    return { tipus, cim, hossz, kiadas, hozzaad, adatok };
+  }
+};
+</script>
