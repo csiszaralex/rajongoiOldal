0f21bcdd399423acf461c8919d943865ed60eb4e
Alex <csiszaralex@gmail.com>
2021-01-12 22:51:47 +0100

    :wastebasket: Use olda mongoose model for frontend

--- src/models/jonasM.ts
+++ src/models/jonasM.ts
@@ -8,50 +8,40 @@ const schemaOptions: mongoose.SchemaOptions = {
 const schema: mongoose.Schema = new mongoose.Schema(
   {
     tipus: {
+      default: 0,
+      required: true,
       type: Number,
-      default: [0, "A típus értéke kötelező, 0 lett az alapérték"],
-      min: [0, "A típus mezőnek legalább 0-nak kell lennie!"],
-      max: [3, "A tpus mező maximum 3 lehet"],
+      min: 0,
+      max: 3,
     },
     cim: {
       type: String,
-      required: [true, "A cím mező kötelező"],
-      unique: [true, "Ilyen cím érték már szerepel az adatbázisban."],
-      validate(this: any, value: any) {
-        return value !== "";
-      },
-      get(val: string) { //. Nagykezdőbetűvel tér vissza
-        return val.charAt(0).toUpperCase() + val.slice(1);
-      },
+      required: true,
+      unique: true,
+      validate: /^(?=\s*\S).*$/,
     },
     hossz: {
       type: Number,
-      min: [0, "A hossz nem lehet negatív szám"],
-      set: Math.round, //? Íráskor egészre kerekítik
+      min: 0,
+      get: Math.round, //? Amikor az értéke beállítódik az kell
     },
     kt: {
       type: String,
-      validate: [
-        function (this: any, value: any) {
-          return value === "-";
-        },
-        'A közreműködő nem lehet "-", ha nincs közreműködő, hagyja üresen a mezőt',
-      ],
     },
     kiadas: {
       type: Number,
-      required: [true, "A kiadas mező kötelező"],
-      min: [1990, "A kiadás nem lehet az együttes megjelenése előtt"],
-      max: [new Date().getFullYear(), "A kiadás nem lehet több, mint a jelenlegi év!"],
+      required: true,
+      min: 1990,
+      max: new Date().getFullYear(),
     },
     kiado: {
       type: String,
     },
     votes: {
-      required: [true, "A votes mező kötelező"],
-      default: [0, "A votes értéke hibás így alapértelmezetten 0 lett"],
+      default: 0,
+      required: true,
       type: Number,
-      min: [0, "A szavazatok száma nem lehet negatív"],
+      min: 0,
     },
   },
   schemaOptions
